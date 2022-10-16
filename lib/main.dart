1cc3736c85cee6056d5e8835c711b06ee08bc206
pallawiwagdre <pallawi1999wagdre@gmail.com>
2022-10-16 21:30:19 +0530

    initial changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,114 @@
-import 'package:child_learning_app/screens/home_screen.dart';
-import 'package:child_learning_app/screens/login_screen.dart';
-import 'package:child_learning_app/screens/registation_screen.dart';
+import 'package:child_learning_app/signgup.dart';
 import 'package:flutter/material.dart';
 
+import 'login.dart';
+
 void main() {
-  runApp(ChildLearningApp());
+  // ignore: prefer_const_constructors
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
-class ChildLearningApp extends StatelessWidget {
-  const ChildLearningApp({super.key});
-
+// ignore: use_key_in_widget_constructors
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Child Learning App",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  // ignore: prefer_const_constructors
+                  Text(
+                    "Welcome to Child Learning App",
+                    style: const TextStyle(
+                        color: Color.fromARGB(255, 6, 78, 202),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40),
+                  ),
+                  // ignore: prefer_const_constructors
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Play gives children chance to practice what they are learning.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 2,
+                // ignore: prefer_const_constructors
+                decoration: BoxDecoration(
+                  // ignore: prefer_const_constructors
+                  image: DecorationImage(
+                      // ignore: prefer_const_constructors
+                      image: AssetImage("assets/login.jpg")),
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.black),
+                        borderRadius: BorderRadius.circular(50)),
+                    // ignore: prefer_const_constructors
+                    child: Text(
+                      "Login",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignUpPage()));
+                    },
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                    child: const Text(
+                      "SignUp",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      home: const HomeScreen(LoginScreen(RegistrationScreen())),
     );
   }
 }
