7b7ce9137a69393a0d38458273c73df359107782
pallawiwagdre <pallawi1999wagdre@gmail.com>
2022-10-17 21:09:40 +0530

    initial changes

--- lib/signgup.dart
+++ lib/signgup.dart
@@ -58,8 +58,8 @@ class SignUpPage extends StatefulWidget {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          height: MediaQuery.of(context).size.height - 50,
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 40),
+          height: MediaQuery.of(context).size.height - 20,
           width: double.infinity,
           child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
